ca888b00c0fd3b7336185b793ba50353a3a6c0d7
Dineth Cooray <dineth@live.com>
2018-05-04 08:13:42 +0530

    first commit

---
+++ src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.services.PushNotificationService;
+
+@Controller
+public class Greeting {
+	private final String TOPIC = "JavaSampleApproach";
+	@Autowired
+	PushNotificationService pushNotificationsService;
+
+	
+	@RequestMapping(value = "/greeting", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<String> greeting() throws JSONException {
+		JSONObject body = new JSONObject();
+		body.put("to", "/topics/" + TOPIC);
+		body.put("priority", "high");
+
+		JSONObject notification = new JSONObject();
+		notification.put("title", "Test Notification");
+		notification.put("body", "Happy Message!");
+		
+		JSONObject data = new JSONObject();
+		data.put("Key-1", "JSA Data 1");
+		data.put("Key-2", "JSA Data 2");
+
+		body.put("notification", notification);
+		body.put("data", data);
+
+		HttpEntity<String> request = new HttpEntity<>(body.toString());
+
+		CompletableFuture<String> pushNotification = pushNotificationsService.send(request);
+		CompletableFuture.allOf(pushNotification).join();
+
+		try {
+			String firebaseResponse = pushNotification.get();
+			
+			return new ResponseEntity<>(firebaseResponse, HttpStatus.OK);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+
+		return new ResponseEntity<>("Push Notification ERROR!", HttpStatus.BAD_REQUEST);
+	}
+
+}
